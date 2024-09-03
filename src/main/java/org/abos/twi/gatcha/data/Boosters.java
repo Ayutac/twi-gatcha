cedaa59aa99692bc964bc9f53e69272ae1422f04
ayutac <fly.high.android@gmail.com>
2024-09-03 20:04:52 +0200

    Added a generic booster.

--- src/main/java/org/abos/twi/gatcha/data/Boosters.java
+++ src/main/java/org/abos/twi/gatcha/data/Boosters.java
@@ -1,4 +1,15 @@
 package org.abos.twi.gatcha.data;
 
+import org.abos.twi.gatcha.core.Booster;
+
+import java.util.List;
+
 public interface Boosters {
+
+    Booster LISCOR_CLASSIC = new Booster(
+            "Classic Liscor",
+            "Summon from Liscor shortly after Erin arrived!",
+            List.of(Characters.ERIN)
+    );
+
 }
