6ae9aa9b8bd283eb304882d05b44e19d40cd754e
ayutac <fly.high.android@gmail.com>
2024-09-03 21:37:13 +0200

    Changed Rarity to Adv Guild Ranks.

--- src/main/java/org/abos/twi/gatcha/core/Rarity.java
+++ src/main/java/org/abos/twi/gatcha/core/Rarity.java
@@ -1,15 +1,28 @@
 package org.abos.twi.gatcha.core;
 
+import org.abos.common.Named;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
 /**
  * The different rarities a {@link CharacterBase} can have.
  */
-public enum Rarity {
+public enum Rarity implements Named {
 
-    COMMON,
-    UNCOMMON,
-    RARE,
-    SUPER_RARE,
-    ULTRA_RARE,
-    SECRET_RARE
+    COMMON("Bronze Rank"),
+    RARE("Silver Rank"),
+    SUPER_RARE("Gold Rank"),
+    ULTRA_RARE("Named Rank");
 
+    private final @NotNull String name;
+
+    Rarity(final @NotNull String name) {
+        this.name = Objects.requireNonNull(name);
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return name;
+    }
 }
