8d91b4685359159f246b885018369bc589d4bf3c
ayutac <fly.high.android@gmail.com>
2024-09-12 19:04:58 +0200

    Improved Main and pom.

--- src/main/java/org/abos/twi/gatcha/Main.java
+++ src/main/java/org/abos/twi/gatcha/Main.java
@@ -1,9 +1,11 @@
 package org.abos.twi.gatcha;
 
+import javafx.application.Platform;
 import org.abos.twi.gatcha.gui.Gui;
 
 public final class Main {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
+        Platform.startup(() -> {});
         Gui.main(args);
     }
 }
