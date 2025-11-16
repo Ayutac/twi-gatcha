fb6fb00309127a98e656b41cbb2f7b5975554f59
Ayutac <fly.high.android@gmail.com>
2025-11-16 07:28:10 +0100

    Fixed saving bug.

--- src/main/java/org/abos/twi/gatcha/core/quest/QuestLineId.java
+++ src/main/java/org/abos/twi/gatcha/core/quest/QuestLineId.java
@@ -1,8 +1,9 @@
 package org.abos.twi.gatcha.core.quest;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public record QuestLineId(QuestKind kind, String stat) {
+public record QuestLineId(QuestKind kind, String stat) implements Serializable {
 
     public QuestLineId {
         Objects.requireNonNull(kind);
